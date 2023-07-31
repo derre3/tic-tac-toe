2b83e891706d3d81626a0fee56f1fb5efd87996d
Andre <123985988+derre3@users.noreply.github.com>
2023-07-31 14:00:52 -0300

    refactor board array to avoid nested loops

--- script.js
+++ script.js
@@ -17,37 +17,22 @@ const player = (name, marker) => {
 const gameBoard = (() => {
   const resetBoard = () => {
     for (let i = 0; i < size; i++) {
-      board[i] = [];
-      for (let j = 0; j < size; j++) {
-        board[i].push(cell);
-      }
+      board[i] = cell;
     }
   };
 
-  const addMarker = (marker, row, column) => {
+  const addMarker = (marker, position) => {
     // check if cell is already occupied
-    if (board[row][column] !== 0) {
+    if (board[position] !== 0) {
       alert("Move not available, please try again"); //Placeholder alert
       gameFlow.playRound();
     }
-    board[row][column] = marker;
+    board[position] = marker;
   };
 
   const getBoard = () => board;
 
-  const flattenBoard = () => {
-    //flattens the 2d board array into a single array
-    const board = gameBoard.getBoard();
-    let arrBoard = [];
-    board.forEach((row) => {
-      row.forEach((cell) => {
-        arrBoard.push(cell);
-      });
-    });
-    return arrBoard;
-  };
-
-  const size = 3;
+  const size = 9;
   const board = [];
   const cell = 0;
   resetBoard();
@@ -56,7 +41,6 @@ const gameBoard = (() => {
     getBoard,
     resetBoard,
     addMarker,
-    flattenBoard,
   };
 })();
 
@@ -79,35 +63,35 @@ const gameFlow = (() => {
   // Check board array for win conditions
   const checkBoard = () => {
     if (
-      (gameBoard.getBoard()[0][0] === activePlayer.getMarker() &&
-        gameBoard.getBoard()[0][1] === activePlayer.getMarker() &&
-        gameBoard.getBoard()[0][2] === activePlayer.getMarker()) || // Win condition #1
-      (gameBoard.getBoard()[0][0] === activePlayer.getMarker() &&
-        gameBoard.getBoard()[1][0] === activePlayer.getMarker() &&
-        gameBoard.getBoard()[2][0] === activePlayer.getMarker()) || // Win condition #2
-      (gameBoard.getBoard()[0][0] === activePlayer.getMarker() &&
-        gameBoard.getBoard()[1][1] === activePlayer.getMarker() &&
-        gameBoard.getBoard()[2][2] === activePlayer.getMarker()) || // Win condition #3
-      (gameBoard.getBoard()[0][2] === activePlayer.getMarker() &&
-        gameBoard.getBoard()[1][2] === activePlayer.getMarker() &&
-        gameBoard.getBoard()[2][2] === activePlayer.getMarker()) || // Win condition #4
-      (gameBoard.getBoard()[0][2] === activePlayer.getMarker() &&
-        gameBoard.getBoard()[1][1] === activePlayer.getMarker() &&
-        gameBoard.getBoard()[2][0] === activePlayer.getMarker()) || // Win condition #5
-      (gameBoard.getBoard()[2][2] === activePlayer.getMarker() &&
-        gameBoard.getBoard()[1][1] === activePlayer.getMarker() &&
-        gameBoard.getBoard()[0][0] === activePlayer.getMarker()) || // Win condition #6
-      (gameBoard.getBoard()[0][1] === activePlayer.getMarker() &&
-        gameBoard.getBoard()[1][1] === activePlayer.getMarker() &&
-        gameBoard.getBoard()[2][1] === activePlayer.getMarker()) || // Win condition #7
-      (gameBoard.getBoard()[1][0] === activePlayer.getMarker() &&
-        gameBoard.getBoard()[1][1] === activePlayer.getMarker() &&
-        gameBoard.getBoard()[1][2] === activePlayer.getMarker()) // Win condition #8
+      (gameBoard.getBoard()[0] === activePlayer.getMarker() &&
+        gameBoard.getBoard()[1] === activePlayer.getMarker() &&
+        gameBoard.getBoard()[2] === activePlayer.getMarker()) || // Win condition #1
+      (gameBoard.getBoard()[0] === activePlayer.getMarker() &&
+        gameBoard.getBoard()[3] === activePlayer.getMarker() &&
+        gameBoard.getBoard()[6] === activePlayer.getMarker()) || // Win condition #2
+      (gameBoard.getBoard()[0] === activePlayer.getMarker() &&
+        gameBoard.getBoard()[4] === activePlayer.getMarker() &&
+        gameBoard.getBoard()[8] === activePlayer.getMarker()) || // Win condition #3
+      (gameBoard.getBoard()[2] === activePlayer.getMarker() &&
+        gameBoard.getBoard()[5] === activePlayer.getMarker() &&
+        gameBoard.getBoard()[8] === activePlayer.getMarker()) || // Win condition #4
+      (gameBoard.getBoard()[2] === activePlayer.getMarker() &&
+        gameBoard.getBoard()[4] === activePlayer.getMarker() &&
+        gameBoard.getBoard()[6] === activePlayer.getMarker()) || // Win condition #5
+      (gameBoard.getBoard()[8] === activePlayer.getMarker() &&
+        gameBoard.getBoard()[7] === activePlayer.getMarker() &&
+        gameBoard.getBoard()[6] === activePlayer.getMarker()) || // Win condition #6
+      (gameBoard.getBoard()[4] === activePlayer.getMarker() &&
+        gameBoard.getBoard()[1] === activePlayer.getMarker() &&
+        gameBoard.getBoard()[7] === activePlayer.getMarker()) || // Win condition #7
+      (gameBoard.getBoard()[4] === activePlayer.getMarker() &&
+        gameBoard.getBoard()[3] === activePlayer.getMarker() &&
+        gameBoard.getBoard()[5] === activePlayer.getMarker()) // Win condition #8
     ) {
-      console.table(gameBoard.getBoard()); // Placeholder display of the board
+      console.log(gameBoard.getBoard()); // Placeholder display of the board
       console.log(activePlayer.getName(), "Wins!!"); //Placeholder log
       return "win";
-    } else if (!gameBoard.flattenBoard().includes(0)) {
+    } else if (!gameBoard.getBoard().includes(0)) {
       // check for available space in the board
       console.log("Draw Game");
       return "draw";
@@ -115,13 +99,9 @@ const gameFlow = (() => {
   };
 
   const playRound = () => {
-    console.table(gameBoard.getBoard()); // Placeholder display of the board
+    console.log(gameBoard.getBoard()); // Placeholder display of the board
     console.log(`${getActivePlayer().getName()} Turn`);
-    gameBoard.addMarker(
-      activePlayer.getMarker(),
-      prompt("Pick a Row"),
-      prompt("Pick a Column")
-    );
+    gameBoard.addMarker(activePlayer.getMarker(), prompt("Pick Position"));
     if (!checkBoard()) {
       switchTurn();
       playRound();
